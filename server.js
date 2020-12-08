1a44c48e48f2b941502d1e687fc051dce8aef4d4
Jay Idrees <idreesjay@gmail.com>
2020-12-08 14:09:17 -0500

    importing axios in react's app.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app=express();
 
 const PORT=process.env.PORT || 4000;
 
+// Setting up a get request to test the server
+app.get("/api/welcome", (req, res)=>{
+    res.json({message:"Welcome to USRA.tech"});
+});
+
 app.listen (PORT, ()=>{
     console.log(`Welcome to USRA.tech, your server is running at https://localhost:${PORT}`)
 })
