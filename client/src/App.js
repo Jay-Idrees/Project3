c10d9a4ffb719d5211e2d5ebcb185e228a0557d1
Jay Idrees <idreesjay@gmail.com>
2020-12-09 06:14:47 -0500

    setup initial get request and using useEffect and useReducer to display a message into a react app using axios to demonstrate that the react app and the express servers are synced. Also added a proxy in the client package.json to mimic the port used for the express server so botht th react app and the server are run on the same port

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,51 @@
+
+// Inserting a hook - We want to load this whenever we want to load this page
+import React, {useEffect, useReducer} from "react"
 import logo from './logo.svg';
 import './App.css';
+// Can later move it out to api.js later
+import axios from "axios";
 
 function App() {
+// Making a state as we have to update the message
+// useReducer is another hook like useeffect- typically used when there is complex state logic that involves multiple sub-values or when a new state depends on the previous one
+  const [state, dispatch]= useReducer((state, action)=>{
+    switch(action.type){
+      case "GET_MESSAGE":
+        return {
+          ...state, 
+          message:action.message
+
+        }// cl br return
+        default:
+          return state;
+    }
+  }, {message: undefined}) // br close reducer. This step is setting up the message
+
+
+  // useEffect hook--------------------------
+useEffect(()=>{
+  // Creating a load message function
+loadMessage();
+}, []) // cl br useEffect
+//---------------------------------------------
+
+const loadMessage=()=>{
+  axios.get("/api/welcome").then(({data})=>{
+    // destructuring message from data
+    const { message} = data;
+    dispatch({type:"GET_MESSAGE", message})
+  })
+} //br cl loadMessage
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+          {state.message ? <p>{state.message} </p>: <p>Edit <code>src/App.js</code> and save to reload. </p>}
+       
         <a
           className="App-link"
           href="https://reactjs.org"
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app=express();
 const PORT=process.env.PORT || 4000;
 
 // Setting up a get request to test the server
+// the get request is from the perspective of the server- the server is getting a request to display res.json when the client types in api/welcome
 app.get("/api/welcome", (req, res)=>{
+
+    // putting an object inside res.json
     res.json({message:"Welcome to USRA.tech"});
 });
 
