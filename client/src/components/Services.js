ec68be63fb895bcb574777f2b1162f0f61466a0a
Jay Idrees <idreesjay@gmail.com>
2020-12-15 08:04:26 -0500

    Created the about and services pages

---
+++ client/src/components/Services.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Headbar from "./Headbar/Headbar";
+
+function Services(){
+    return(
+     
+<Container>
+<Headbar />
+  <Row>
+    <Col sm={8}>
+       <p>
+       
+       USRA tech offers the services of statistical analysis for Medical research including propensity score matching, regression analysis, survival data analysis with cox propotional hazard analysis and analylysis of the longitudinal data. 
+
+
+       </p>
+
+       </Col>
+     
+          <Col sm={4}>sm=4</Col>
+  </Row>
+  </Container>
+
+    )
+}
+
+export default Services
